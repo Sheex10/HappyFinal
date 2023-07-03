a0c156f6292da383c3720c41e4ad2d2b1cdfaca1
Felipe <feliperivas1709@gmail.com>
2023-07-03 00:34:23 -0400

    Perros
    html perros listo!

--- ApUno/urls.py
+++ ApUno/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, Gatos, Perros
+from .views import home, Gatos, Perros, buscar_interno_producto
 
 urlpatterns = [
     path('', home,name="home"),
     path('Gatos', Gatos,name="Gatos"),
     path('Perros', Perros,name="Perros"),
+    path('buscar_interno_producto/<int:id>',buscar_interno_producto,name="buscar_interno_producto"),
 ]
--- ApUno/views.py
+++ ApUno/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Producto, Usuario, Categoria
 
 # Create your views here.
 
@@ -6,8 +7,19 @@ def home(request):
     return render(request, 'ApUno/home.html')
 
 def Perros(request):
-    return render(request, 'ApUno/Perros.html')
+    listaProductos = Producto.objects.filter(categoria = 4)
+    contexto={
+        "nomProd": listaProductos
+
+    }
+    return render(request,'ApUno/Perros.html', contexto)
 
 def Gatos(request):
     return render(request, 'ApUno/Gatos.html')
 
+def buscar_interno_producto(request,id):
+    prod = Producto.objects.get(id_producto = id)
+    contexto = {
+        "nombree": prod
+    }
+    return render(request,'ApUno/Correa.html',contexto)
