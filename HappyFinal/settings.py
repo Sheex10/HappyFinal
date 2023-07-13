7e12048560920465794f4a4f668e83ba61b6596f
Felipe <feliperivas1709@gmail.com>
2023-07-12 23:48:00 -0400

    xddddd
    si

--- HappyFinal/settings.py
+++ HappyFinal/settings.py
@@ -85,8 +85,8 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/orcl',
         'USER': 'exafinal',
         'PASSWORD': '123456',
         'TEST': {
